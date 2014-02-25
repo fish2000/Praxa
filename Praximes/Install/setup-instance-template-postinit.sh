372dc580a856b19a87b28c3ee0d34ebe86fe04fc
Alexander Böhn <fish2000@gmail.com>
2014-02-25 17:54:07 -0500

    Praximic lambda-esque variable indicates an environment's status, or lack thereof, as deployed

--- Praximes/Install/setup-instance-template-postinit.sh
+++ Praximes/Install/setup-instance-template-postinit.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
 cd $VIRTUAL_ENV
-echo "+ Setting up supervisord.conf ..."
+echo "+ Setting up supervisord.conf and supervisord-deploy.conf ..."
 bin/viron ${TEMPLATES}/supervisord.conf > $SUPERVISORD_CONF
+bin/viron ${TEMPLATES}/supervisord.conf > $SUPERVISORD_DEPLOY_CONF
 
-echo "+ Setting up Procfile ..."
-bin/viron ${TEMPLATES}/Procfile > $PROCFILE
+echo "+ Setting up uwsgi.ini and uwsgi-deploy.ini ..."
+bin/viron ${TEMPLATES}/uwsgi.ini > $UWSGI_INI
+bin/viron ${TEMPLATES}/uwsgi.ini > $UWSGI_DEPLOY_INI
+
+echo "+ Setting up development and deployment Procfiles ..."
+bin/viron ${TEMPLATES}/Procfile > $PROCFILE_DEVELOP
+bin/viron ${TEMPLATES}/Procfile > $PROCFILE_DEPLOY
+if [[ $DEPLOYED ]]; then
+    ln -s $PROCFILE_DEPLOY $PROCFILE
+else
+    ln -s $PROCFILE_DEVELOP $PROCFILE
+fi
 
 echo "+ Setting up Makefile ..."
 bin/viron ${TEMPLATES}/Makefile > $MAKEFILE
 
-echo "+ Setting up redis ..."
+echo "+ Setting up redis.conf and redis-deploy.conf ..."
 bin/viron ${TEMPLATES}/redis.conf > $INSTANCE_REDIS_CONF
+bin/viron ${TEMPLATES}/redis.conf > $INSTANCE_REDIS_DEPLOY_CONF
 
 echo "+ Preparing the executable Django admin script ..."
 DJANGO_ROOT=`bin/python -c "import django,os;print os.path.dirname(django.__file__)"`
@@ -20,3 +32,9 @@ chmod +x $DJANGO_ADMIN_SCRIPT
 chmod g+x $DJANGO_ADMIN_SCRIPT
 export DJANGO_ADMIN="${INSTANCE_BIN}/$(basename $DJANGO_ADMIN_SCRIPT)"
 test ! -r $DJANGO_ADMIN && ln -s $DJANGO_ADMIN_SCRIPT $DJANGO_ADMIN
+
+echo "+ Preparing README.md stub ..."
+# This next bit here is how bash does markdown
+INSTANCE_H1=`echo $INSTANCE_NAME | sed -e s/[A-Za-z0-9_-]/=/g`
+YEAR=`date '+%Y'`
+bin/viron ${TEMPLATES}/README.stub.md > $README
---
+++ Praximes/Templates/README.stub.md
@@ -0,0 +1,22 @@
+$INSTANCE_NAME
+$INSTANCE_H1
+
+The TESSAR codebase is made only with the finest certified-organic Praxis<sup>&reg;</sup> &mdash; the synthesis of theory and practice &mdash; 100% hand-crafted with pride here in the United States of America.
+
+The Praxis<sup>&reg;</sup> initiatives are brought to you by OST, LLC &ndash; the makers of Praxa Projects. Work on a Praxa Project today and Taste The Difference.&trade; Download the open-source Praxa repo from your local GitHub<sup>&reg;</sup> server, absolutely free<b>*</b>, and start making your own Praximes&trade; to use or trade with your friends:
+
+* [https://github.com/fish2000/Praxa](https://github.com/fish2000/Praxa)
+
+The Praxis&reg; initiatives are also funded in part by generous IP grants from our donors:
+
+* The Internet
+* The Theo de Raadt Opera House Endowment
+* GLG Footwear of Midtown
+* Laserdisc for Her&trade;
+* Richard Stallman Allied Industrial Partners Worldwide
+
+And, as always, by Programmers Like You.
+
+&nbsp;
+
+<small><b>* Standard data rates and jurisdictional software-patent operational risks may apply.</b> GitHub<sup>&reg;</sup> is a registered trademark of the estate of Dr. Gitton Hubrick, III. All Other Rights Reserved. &copy; $YEAR OST, LLC. Void where prohibited by law or forward declaration.</small>
